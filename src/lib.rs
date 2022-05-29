bf1a2c2bb206799ed37a1bf69fb14dea725bba52
Clinton Selke <clinuxrulz@gmail.com>
2022-05-29 20:11:27 +1000

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub kotlin); // synthesized by LALRPOP
+
+#[test]
+fn kotlin1() {
+    assert!(kotlin::TermParser::new().parse("22").is_ok());
+    assert!(kotlin::TermParser::new().parse("(22)").is_ok());
+    assert!(kotlin::TermParser::new().parse("((((22))))").is_ok());
+    assert!(kotlin::TermParser::new().parse("((22)").is_err());
+}
