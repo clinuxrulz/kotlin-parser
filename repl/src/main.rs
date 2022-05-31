3c7c9b54f62cbb303460fa36a675510c7b22f81b
Clinton Selke <clinuxrulz@gmail.com>
2022-05-31 12:03:26 +1000

    repl

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,12 +1,27 @@
 use kotlin_parser::kotlin;
+use std::io::Write;
+
+fn trim_newline(s: &mut String) {
+    if s.ends_with('\n') {
+        s.pop();
+        if s.ends_with('\r') {
+            s.pop();
+        }
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let mut buffer = String::new();
     loop {
+        print!("> ");
+        std::io::stdout().flush()?;
         std::io::stdin().read_line(&mut buffer)?;
-        if buffer == "quit" {
+        trim_newline(&mut buffer);
+        println!("{}", buffer);
+        if buffer == "quit" || buffer == "exit" {
             break;
         }
+        buffer.clear();
     }
     Ok(())
 }
