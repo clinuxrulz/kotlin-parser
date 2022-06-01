e43f50c170abe1d29b8297cd9da5077092c6604c
Clinton Selke <clinuxrulz@gmail.com>
2022-06-01 20:17:14 +1000

    SimpleUserType

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -12,7 +12,7 @@ fn trim_newline(s: &mut String) {
 
 fn main() -> std::io::Result<()> {
     let mut buffer = String::new();
-    let parser = kotlin::TypeArgumentsParser::new();
+    let parser = kotlin::SimpleUserTypeParser::new();
     loop {
         print!("> ");
         std::io::stdout().flush()?;
